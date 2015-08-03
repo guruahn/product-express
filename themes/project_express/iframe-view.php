f03cf9948bf404178e16cdebd54cee5fa3c2b37d
guruahn <guruahn@gmail.com>
2015-08-03 15:42:19 +0900

    - 아이프레임뷰 페이지 공유 링크 설정. Signed-off-by: guruahn <guruahn@gmail.com>

--- themes/project_express/iframe-view.php
+++ themes/project_express/iframe-view.php
@@ -40,7 +40,7 @@ $blog_posts = new WP_Query( $query );
 
                     <div class="comments"><a href="<?php echo get_permalink();?>"><i class="fa fa-comment"></i> 댓글 <span class="fb-comments-count" data-href="<?php echo get_permalink();?>"></span>개</a> <span class="spacer">/</span>
 
-                    <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+                    <div class="fb-like" data-href="<?php echo get_permalink();?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
                     </div>
                 </div>
                 <div id="view" style=""><iframe  src="<?php echo $link;?>" class="iframeView" frameborder="0"></iframe></div>
