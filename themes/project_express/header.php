02cfc88359b7a5d5eebc94be1deda0562ace9a9f
guruahn <guruahn@gmail.com>
2015-08-11 15:22:36 +0900

    - 헤더의 title 스타일 변경. Signed-off-by: guruahn <guruahn@gmail.com>

--- themes/project_express/header.php
+++ themes/project_express/header.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title><?php wp_title( ' | ', true, 'right' ); ?></title>
+    <title><?php wp_title( ' - ', true, 'right' ); ?></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
