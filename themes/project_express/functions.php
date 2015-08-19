a09680587999a7f4f20164347e5f7f82c59dd026
guruahn <guruahn@gmail.com>
2015-08-19 15:57:10 +0900

    - 영상 썸네일에 레이어작업을 위한 마크업 변경. Signed-off-by: guruahn <guruahn@gmail.com>

--- themes/project_express/functions.php
+++ themes/project_express/functions.php
@@ -437,7 +437,7 @@ if ( ! function_exists( 'product_express_get_movie_thumbnail_by_url' ) ) :
             maxresdefault : Maximum Resolution Thumbnail (1920x1080 pixels)
             */
             $youtube_id = product_express_get_youtube_id($url);
-            $image = '<img class="attachment-thumbnail wp-post-image action video" width="401" height="264" src="http://img.youtube.com/vi/' . $youtube_id . '/' . $size .'.jpg" alt="'.$alt.'">';
+            $image = '<div class="video"><img class="attachment-thumbnail wp-post-image action" width="401" height="264" src="http://img.youtube.com/vi/' . $youtube_id . '/' . $size .'.jpg" alt="'.$alt.'"></div>';
             $image .= '<div class="popup"><i class="fa fa-times close"></i><div class="vimeo"><iframe class="movie-iframe" width="560" height="315" src="https://www.youtube.com/embed/'. $youtube_id .'?wmode=transparent" frameborder="0" allowfullscreen></iframe></div></div>';
         }elseif(strpos($url, 'vimeo') !== false) {
             $vimeo_id = product_express_get_vimeo_info_by_url($url);
